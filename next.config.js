959761c33e7ef63f82387473e6ca7bed000195cc
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-06-23 11:25:46 +0200

    first commit to test todolist

--- next.config.js
+++ next.config.js
@@ -4,3 +4,10 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// module.exports = {
+//   env: {
+//     MONGO_URI:
+//       'mongodb+srv://Mike:CAnNyUVzTyBAn3pD@cluster0.e6hre.mongodb.net/?retryWrites=true&w=majority'
+//   }
+// };
